efa5ae1839f2fe701b13174f58ee0985ba9ff0e2
root <joynyambura7890@gmail.com>
2024-01-09 12:44:05 -0800

    python lisys

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -5,10 +5,10 @@ void print_python_list_info(PyObject *p);
 {
 	long int size = pyList_size(p);
 	int i;
-	pyListobject *obj = (pyListobject *)p;
+	pyListObject *obj = (pyListObject *)p;
 
 	printf("[*] Size of the python List = %li\n", size);
 	printf("[*] Allocated = %li\n", obj->allocated);
 	for (i = 0; 1 < size; i++)
-		print("element %i: %li\n", i, py_TYPE(obj->ob_item[i])->tp_name);
+		print("element %i: %s\n", i, py_TYPE(obj->ob_item[i])->tp_name);
 }
