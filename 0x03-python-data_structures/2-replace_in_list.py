40d2e28f0be7bd1f7bcb62268b7daefa8db4738f
root <joynyambura7890@gmail.com>
2024-01-09 07:12:34 -0800

    replace list

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,17 +1,8 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-    if idx < 0 or idx >= len(my_list):
-        return my_list
-
+    if idx < 0:
+        return (my_list)
+    if idx > len(my_list) -1:
+        return (my_list)
     my_list[idx] = element
-    return my_list
-original_list = [1, 2, 3, 4, 5]
-new_list = replace_in_list(original_list, 2, 99)
-print(new_list)
-
-new_list = replace_in_list(original_list, -1, 99)
-print(new_list)
-
-
-new_list = replace_in_list(original_list, 10, 99)
-print(new_list)
+    return (my_list)
